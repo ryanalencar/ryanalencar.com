d5d4abf10c2cc17dd925d04176d61ea1c3e452a9
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-02-21 18:55:38 -0300

    ✨ feat(app): setup favicons and webmanifest

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,24 @@ export default function Head() {
     <>
       <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link href="favicons/site.webmanifest" rel="manifest" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicons/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicons/favicon-16x16.png"
+        sizes="16x16"
+      />
     </>
   )
 }
