ad82d08845ea7544765c0c37a056a3b449389d7a
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-02-21 02:28:42 -0300

    ✨ feat(app): loading page on main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,15 @@ import { NEXT_SEO_DEFAULT } from '@/config/SEO'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html>
       <head>
         <NextSeo useAppDir {...NEXT_SEO_DEFAULT} />
       </head>
-      <body>{children}</body>
+      <body>
+        <h1>Header</h1>
+        {children}
+        <h1>Footer</h1>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
-export default function Home() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const toDo = await response.json()
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>ToDo:</h1>
+      <pre>{JSON.stringify(toDo, null, 2)}</pre>
     </div>
   )
 }
